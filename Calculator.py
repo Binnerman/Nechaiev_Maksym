7ee8e3a746cf31c51ba232fa28083484b69425d2
Binnerman <nechaievmaksym@gmail.com>
2023-02-25 00:30:44 +0200

    Cycle addition

--- Calculator.py
+++ Calculator.py
@@ -1,43 +1,43 @@
 first = 0
 second = 0
 act = 0
-result = " "
+result = ""
+again = "Так"
 
-try:
-    first = float(input('Введіть перше число:'))
-except ValueError:
+while again == "Так":
+    try:
+        first = float(input('Введіть перше число:'))
+    except ValueError:
         try:
             first = float(input('Підходить тільки числове значення:'))
         except ValueError:
             print('Я так рахувати не вмію!')
             quit()
-
-act = input('Вкажіть математичну дію на вибір: "+" "-" "*" "/" "^"\nВаш вибір:')
-
-try:
-    second = float(input('Введіть друге число:'))
-except ValueError:
+    act = input('Вкажіть математичну дію на вибір: "+" "-" "*" "/" "^"\nВаш вибір:')
+    try:
+        second = float(input('Введіть друге число:'))
+    except ValueError:
         try:
             second = float(input('Підходить тільки числове значення:'))
         except ValueError:
             print('Я так рахувати не вмію!')
             quit()
 
-try:
-    if act == '+':
-        result = first + second
-    elif act == '-':\
-        result = first - second
-    elif act == '*':\
-        result = first * second
-    elif act == '/':\
-        result = first / second
-    elif act == "^":\
-        result = first ** second
-    else:
-        print("Неправильно обрана дія")
-except ArithmeticError:
-    print("На нуль ділити не можна - всесвіт сколлапсується!")
+    try:
+        if act == '+':
+            result = first + second
+        elif act == '-':
+            result = first - second
+        elif act == '*':
+            result = first * second
+        elif act == '/':
+            result = first / second
+        elif act == "^":
+            result = first ** second
+        else:
+            print("Неправильно обрана дія")
+    except ArithmeticError:
+        print("На нуль ділити не можна - всесвіт сколлапсується!")
 
-print(result)
-
+    print(result)
+    again = input("Якщо бажаєте продовжити напишіть 'Так':")

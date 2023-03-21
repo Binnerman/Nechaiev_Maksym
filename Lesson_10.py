0acfac869617bae3e65d97912e35c8656e2e2472
Binnerman <nechaievmaksym@gmail.com>
2023-03-21 17:34:41 +0200

    HomeTask_10

--- Lesson_10.py
+++ Lesson_10.py
@@ -46,17 +46,63 @@ names = "names.txt"
 # у яких date - це дата з рядка (якщо є),
 # Наприклад [{"date": "1st January 1919"}, {"date": "8th February 1828"}, ...]
 
+#
+# def dateination(name=autors):
+#     with open(name, "r") as my_file:
+#         data = my_file.readlines()
+#         my_list = []
+#         for i in list(data):
+#             x = i.split(" - ")
+#             if "\n" not in x[0]:
+#                 y = {"date": x[0]}
+#                 my_list.append(y)
+#
+#         return my_list
+#
+# # print(dateination())
 
-def dateination(name=autors):
-    with open(name, "r") as my_file:
-        data = my_file.readlines()
-        my_list = []
-        for i in list(data):
-            x = i.split(" - ")
-            if "\n" not in x[0]:
-                y = {"date": x[0]}
-                my_list.append(y)
 
-        return my_list
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# 4* (*здавати не обов'язково).
+# Написати функцію, яка отримує у вигляді параметра ім'я файлу (authors.txt) та повертає список
+# словників виду {"date_original": date_original, "date_modified": date_modified}
+# у яких date_original - це дата з рядка (якщо є),
+# а date_modified - ця ж дата, представлена у форматі "dd/mm/yyyy" (d-день, m-місяць, y-рік)
+# Наприклад [{"date_original": "8th February 1828", "date_modified": 08/02/1828}, ...]
 
-print(dateination())
+# from datetime import datetime
+#
+# x = '1st January 1919'
+# x1 = x.replace("st", "")
+# x2 = x1.replace(" ", "/")
+# print(x2)
+#
+# f = datetime.strptime(x2, "%d/%B/%Y")
+# d = datetime.strftime(f, "%d/%m/%Y")
+# print(d)
+
+
+# def dateination(name=autors):
+#     from datetime import datetime
+#     with open(name, "r") as my_file:
+#         data = my_file.readlines()
+#         my_list = []
+#         for i in list(data):
+#             x = i.split(" - ")
+#             if "\n" not in x[0]:
+#                 f = x[0].split()
+#                 f[0] = f[0].replace("st", "").replace("th", "").replace("nd", "").replace("rd", "")
+#                 date = "/".join(f)
+#                 if date.split("/")[0].isdecimal():
+#                     fdate = datetime.strptime(date, "%d/%B/%Y")
+#                     f2date = fdate.strftime("%d/%m/%Y")
+#                 else:
+#                     fdate = datetime.strptime(date, "%B/%Y")
+#                     f2date = fdate.strftime("%m/%Y")
+#                 print(f2date)
+#                 y = {"date_original": x[0], "date_modified": f2date, }
+#                 my_list.append(y)
+#
+#         return my_list
+#
+# print(dateination())
